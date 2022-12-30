7f6747800e11eb5ff2e41c082cdfa8c6364825f1
Julian <juliaximo@gmail.com>
2022-12-30 04:46:27 -0300

    Web security configuration created

---
+++ src/main/java/com/argprog/portfolio/security/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.argprog.portfolio.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ *
+ * @author Usuario
+ */
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+          .disable()
+          .authorizeHttpRequests()
+          .requestMatchers(HttpMethod.DELETE)
+          .hasRole("ADMIN")
+          .requestMatchers("/admin/**")
+          .hasAnyRole("ADMIN")
+          .requestMatchers("/user/**")
+          .hasAnyRole("USER", "ADMIN")
+          .requestMatchers("/login/**")
+          .anonymous()
+          .anyRequest()
+          .authenticated()
+          .and()
+          .httpBasic()
+          .and()
+          .sessionManagement()
+          .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+    /*@Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.debug(securityDebug)
+          .ignoring()
+          .antMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/favicon.ico");
+    }*/
+    
+}
