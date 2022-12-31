4c1cbf2c1ca6c3e150a44344532fa0fb9bbde77e
Julian <juliaximo@gmail.com>
2022-12-31 18:56:13 -0300

    Created Authentication Entry Point

---
+++ src/main/java/com/argprog/portfolio/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.argprog.portfolio.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+    
+    private static final long serialVersionUID = 5234725241L;
+    
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException{
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
