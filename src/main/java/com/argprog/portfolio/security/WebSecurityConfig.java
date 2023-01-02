44ef6500a81835140e2954d3aa2881e928a38b95
Julian <juliaximo@gmail.com>
2023-01-02 02:00:28 -0300

    Custom authentication manager created, bugs fixed

--- src/main/java/com/argprog/portfolio/security/WebSecurityConfig.java
+++ src/main/java/com/argprog/portfolio/security/WebSecurityConfig.java
@@ -4,12 +4,18 @@
  */
 package com.argprog.portfolio.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -19,10 +25,15 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity(debug=true)
 @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@Configuration
 public class WebSecurityConfig {
+    
+    @Autowired
+    JwtAuthenticationEntryPoint authEntry;
+    
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf()
+        /*http.csrf()
           .disable()
           .authorizeHttpRequests()
           .requestMatchers(HttpMethod.DELETE)
@@ -39,10 +50,31 @@ public class WebSecurityConfig {
           .httpBasic()
           .and()
           .sessionManagement()
-          .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+          .sessionCreationPolicy(SessionCreationPolicy.STATELESS);*/
+        http.csrf()
+            .disable()
+            .authorizeHttpRequests()
+            .requestMatchers("/authenticate").permitAll().
+            // all other requests need to be authenticated
+            anyRequest().authenticated().and()
+            // make sure we use stateless session; session won't be used to
+            // store user's state.
+            .exceptionHandling()
+            .authenticationEntryPoint(authEntry).and().sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
     }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailsService userDetailsService) 
+        throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                   .userDetailsService(userDetailsService)
+                   .passwordEncoder(bCryptPasswordEncoder)
+                   .and()
+                   .build();
+}
 
     /*@Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
