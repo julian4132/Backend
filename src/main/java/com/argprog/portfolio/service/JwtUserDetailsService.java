6201c31855539827b327c6d9b1708a621ce68110
Julian <juliaximo@gmail.com>
2023-01-02 16:42:19 -0300

    Missing @Service import

--- src/main/java/com/argprog/portfolio/service/JwtUserDetailsService.java
+++ src/main/java/com/argprog/portfolio/service/JwtUserDetailsService.java
@@ -9,18 +9,20 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author Usuario
  */
+@Service
 public class JwtUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         
         // to do;
-        return new User("ExampleUsername", "ExampleToken", new ArrayList<>());
+        return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6", new ArrayList<>());
     }
     
 }
