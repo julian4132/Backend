0580377d1c2d8e06ae90756596973686ae12c62d
Julian <juliaximo@gmail.com>
2023-01-06 01:33:01 -0300

    Created Refresh Token DAO

---
+++ src/main/java/com/argprog/portfolio/model/RefreshTokenDAO.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.argprog.portfolio.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import java.time.Instant;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.UUID;
+
+/**
+ *
+ * @author Usuario
+ */
+@Entity(name="refresh_token")
+@Setter @Getter
+@NoArgsConstructor
+public class RefreshTokenDAO {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long Id;
+    
+    @OneToOne(optional=false, cascade=CascadeType.ALL)
+    @JoinColumn(name="user_id", unique=true)
+    private String username;
+    
+    @Column
+    private Instant expiry;
+    
+    @Column
+    private String secret;
+    
+    public RefreshTokenDAO(Long Id, String username, Instant expiry){
+        this.Id=Id;
+        this.username=username;
+        this.expiry=expiry;
+        this.secret=UUID.randomUUID().toString();
+    }
+}
