f539d46913346479d7ce6c18e015fbb5299c93b0
Julian <juliaximo@gmail.com>
2023-01-08 00:35:38 -0300

    Replaced hardcoded CrossOrigin by value in application.properties

--- src/main/java/com/argprog/portfolio/controller/JWTAuthController.java
+++ src/main/java/com/argprog/portfolio/controller/JWTAuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins = {"${settings.cors_origin}"})
 public class JWTAuthController {
     
     @Autowired
--- src/main/java/com/argprog/portfolio/controller/UserDataController.java
+++ src/main/java/com/argprog/portfolio/controller/UserDataController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins = {"${settings.cors_origin}"})
 public class UserDataController {
     
     private final FetchDataService fetchDataService;
